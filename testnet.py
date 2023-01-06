16c1c40721fce33bf5bc380b2fdeef6d1e2a7259
ZirLec9527 <109225871+ZirLec9527@users.noreply.github.com>
2023-01-07 04:20:08 +0800

    Delete testnet.py

--- testnet.py
+++
@@ -1,5 +0,0 @@
-WEBHOOK_TOKEN = 'test1234qwer' # some random key you generate
-EXCHANGE = '' # ccxt library support list on https://docs.ccxt.com/en/latest/exchange-markets.html
-API_KEY = '' # your exchange api key
-API_SECRET = '' # your exchange api secret
-PASSWORD = '' # some exchange need. like OKX
