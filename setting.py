7f86a84543b9beb8adf9248cea063db1bfff30b9
ZirLec9527 <109225871+ZirLec9527@users.noreply.github.com>
2023-01-07 04:20:57 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,5 +1,20 @@
-WEBHOOK_TOKEN = '' # some random key you generate
-EXCHANGE = '' # ccxt library support list on https://docs.ccxt.com/en/latest/exchange-markets.html
-API_KEY = '' # your exchange api key
-API_SECRET = '' # your exchange api secret
-PASSWORD = '' # some exchange need. like OKX
+account = {
+    'WEBHOOK_TOKEN': '', # some random key you generate
+    'EXCHANGE': '', # ccxt library support list on https://docs.ccxt.com/en/latest/exchange-markets.html
+    'API_KEY': '', # your exchange api key
+    'API_SECRET': '', # your exchange api secret
+    'API_PASSWORD': '' # some exchange need. like OKX
+}
+
+testnet = {
+    'WEBHOOK_TOKEN': 'test1234qwer', # some random key you generate
+    'EXCHANGE': '', # ccxt library support list on https://docs.ccxt.com/en/latest/exchange-markets.html
+    'API_KEY': '', # your exchange api key
+    'API_SECRET': '', # your exchange api secret
+    'API_PASSWORD': '' # some exchange need. like OKX
+}
+# add your ip
+iplist = [ 
+    '1.2.3.4',
+    '5.6.7.8'
+]
